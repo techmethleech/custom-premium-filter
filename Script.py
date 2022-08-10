c4dba0727df52d910fab4e79b23d4b124c00db6e
techmethleech <100306770+techmethleech@users.noreply.github.com>
2022-08-11 01:50:09 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,6 +1,10 @@
 class script(object):
-    START_TXT = """𝙷𝙴𝙻𝙾 {},
-𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍"""
+    START_TXT = """<b>Hello {}🙋‍♂️,</b>
+<b>My Name Is <a href=https://t.me/{}>{}</a>,</b>
+<b>I'm Powerful Auto-Filter Bot You Can Use Me As A Auto-filter Bot In Your Group ..</b>
+<b>Its Easy To Use Me; Just Add Me To Your Group As Admin, Thats All, i will Provide Movies There...🤓</b>
+<b>⚠️More Help Check Help Button Below</b>
+<b>©️MᴀɪɴᴛᴀɪɴᴇD Bʏ <a href=https://t.me/TechGuideLKChannel>TechGuideLK</a></b>"""
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
