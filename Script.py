97044ffdee259310b47bc572147a5ffb67ae519b
techmethleech <100306770+techmethleech@users.noreply.github.com>
2022-08-11 01:35:51 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -9,6 +9,7 @@ class script(object):
 <b>⚠️More Help Check Help Button Below</b>
 
 <b>©️MᴀɪɴᴛᴀɪɴᴇD Bʏ <a href=https://t.me/TechGuideLKChannel>TechGuideLK</a></b>"""
+        
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
